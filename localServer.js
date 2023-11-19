c0bddd6c0b1e3448e139ac050aed85cdce5cdddc
Dinoosauro <80783030+Dinoosauro@users.noreply.github.com>
2023-11-19 21:11:26 +0100

    Add localServer support for certificates

--- localServer.js
+++ localServer.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fs = require("fs");
 const customHeadersAppLevel = function (req, res, next) {
     res.append("Cross-Origin-Embedder-Policy", "require-corp");
     res.append("Cross-Origin-Opener-Policy", "same-origin");
@@ -7,4 +8,11 @@ const customHeadersAppLevel = function (req, res, next) {
  };
  app.use(customHeadersAppLevel);
 app.use(express.static(__dirname + '/dist'));
-app.listen(3000, () => console.log('Listening on port 3000'));
+if (fs.existsSync("key.pem") && fs.existsSync("cert.pem")) {
+    require("https").createServer({
+        key: fs.readFileSync("key.pem", "utf-8"),
+        cert: fs.readFileSync("cert.pem", "utf-8")
+    }, app).listen(3000, () => console.log('Listening on port 3000'));    
+} else {
+    app.listen(3000, () => console.log('Listening on port 3000'));
+}
